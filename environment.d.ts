482ed0cc8111eb2c6672218f7456da5a6a642ba6
developer-256 <ch256.it@gmail.com>
2025-02-01 12:52:01 +0500

    Initial commit

---
+++ environment.d.ts
@@ -0,0 +1,10 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXTAUTH_SECRET: string;
+
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+
+    NEXT_PUBLIC_BACKEND_BASE_URL: string;
+  }
+}
