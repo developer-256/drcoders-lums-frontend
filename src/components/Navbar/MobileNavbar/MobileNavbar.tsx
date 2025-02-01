24800053b57be1378d7b7dce73e042c2e6f53df8
developer-256 <ch256.it@gmail.com>
2025-02-01 17:58:54 +0500

    Update MobileNavbar.tsx

--- src/components/Navbar/MobileNavbar/MobileNavbar.tsx
+++ src/components/Navbar/MobileNavbar/MobileNavbar.tsx
@@ -3,17 +3,8 @@ import React from "react";
 import MaxContainer from "../../MaxContainer";
 import NavLogo from "../NavLogo";
 import { Menu, ShoppingBag, UserCircle } from "lucide-react";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
 import { useModal } from "@/app/Stores/ModalStore";
 import NavSearch from "../NavSearch";
-import NavCart from "../NavCart";
 
 const MobileNavbar = () => {
   const open = useModal((state) => state.activeModal === "cart");
@@ -25,30 +16,13 @@ const MobileNavbar = () => {
         <NavLogo />
 
         <div className="flex items-center gap-4">
-          {/* <NavUserIcon /> */}
           <UserCircle size={25} className="text-primary" />
           <ShoppingBag
             size={24}
             className="text-primary"
             onClick={() => setOpen(!open)}
           />
-          {/* <NavCart /> */}
-
-          {/* <Sheet open={open} onOpenChange={setOpen}>
-            <SheetTrigger> */}
           <Menu size={26} className="text-primary" />
-          {/* </SheetTrigger>
-
-            <SheetContent>
-              <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
-                <SheetDescription>
-                  This action cannot be undone. This will permanently delete
-                  your account and remove your data from our servers.
-                </SheetDescription>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet> */}
         </div>
       </div>
 
