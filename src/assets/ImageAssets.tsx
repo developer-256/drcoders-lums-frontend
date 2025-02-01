78abca2f30ca4634ff01b3c8dd0e51707935cf99
developer-256 <ch256.it@gmail.com>
2025-02-01 13:45:14 +0500

    added dummy images

---
+++ src/assets/ImageAssets.tsx
@@ -0,0 +1,57 @@
+export const ImageAssets = {
+  // prettier-ignore
+  prodImgs: {
+    Img0, Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10, Img11, Img12, Img13, Img14, Img15, Img16, Img17, Img18, Img19, Img20, Img21, Img22, Img23, Img24, Img25, Img26, Img27, Img28, Img29, Img30, Img31, Img32, Img33, Img34, Img35, Img36, Img37, Img38, Img39, Img40, Img41, Img42, Img43, Img44, Img45, Img46, Img47, Img48, Img49,
+  },
+};
+
+import Img0 from "@/assets/prod-imgs/0.jpg";
+import Img1 from "@/assets/prod-imgs/1.jpg";
+import Img2 from "@/assets/prod-imgs/2.jpg";
+import Img3 from "@/assets/prod-imgs/3.jpg";
+import Img4 from "@/assets/prod-imgs/4.jpg";
+import Img5 from "@/assets/prod-imgs/5.jpg";
+import Img6 from "@/assets/prod-imgs/6.jpg";
+import Img7 from "@/assets/prod-imgs/7.jpg";
+import Img8 from "@/assets/prod-imgs/8.jpg";
+import Img9 from "@/assets/prod-imgs/9.jpg";
+import Img10 from "@/assets/prod-imgs/10.jpg";
+import Img11 from "@/assets/prod-imgs/11.jpg";
+import Img12 from "@/assets/prod-imgs/12.jpg";
+import Img13 from "@/assets/prod-imgs/13.jpg";
+import Img14 from "@/assets/prod-imgs/14.jpg";
+import Img15 from "@/assets/prod-imgs/15.jpg";
+import Img16 from "@/assets/prod-imgs/16.jpg";
+import Img17 from "@/assets/prod-imgs/17.jpg";
+import Img18 from "@/assets/prod-imgs/18.jpg";
+import Img19 from "@/assets/prod-imgs/19.jpg";
+import Img20 from "@/assets/prod-imgs/20.jpg";
+import Img21 from "@/assets/prod-imgs/21.jpg";
+import Img22 from "@/assets/prod-imgs/22.jpg";
+import Img23 from "@/assets/prod-imgs/23.jpg";
+import Img24 from "@/assets/prod-imgs/24.jpg";
+import Img25 from "@/assets/prod-imgs/25.jpg";
+import Img26 from "@/assets/prod-imgs/26.jpg";
+import Img27 from "@/assets/prod-imgs/27.jpg";
+import Img28 from "@/assets/prod-imgs/28.jpg";
+import Img29 from "@/assets/prod-imgs/29.jpg";
+import Img30 from "@/assets/prod-imgs/30.jpg";
+import Img31 from "@/assets/prod-imgs/31.jpg";
+import Img32 from "@/assets/prod-imgs/32.jpg";
+import Img33 from "@/assets/prod-imgs/33.jpg";
+import Img34 from "@/assets/prod-imgs/34.jpg";
+import Img35 from "@/assets/prod-imgs/35.jpg";
+import Img36 from "@/assets/prod-imgs/36.jpg";
+import Img37 from "@/assets/prod-imgs/37.jpg";
+import Img38 from "@/assets/prod-imgs/38.jpg";
+import Img39 from "@/assets/prod-imgs/39.jpg";
+import Img40 from "@/assets/prod-imgs/40.jpg";
+import Img41 from "@/assets/prod-imgs/41.jpg";
+import Img42 from "@/assets/prod-imgs/42.jpg";
+import Img43 from "@/assets/prod-imgs/43.jpg";
+import Img44 from "@/assets/prod-imgs/44.jpg";
+import Img45 from "@/assets/prod-imgs/45.jpg";
+import Img46 from "@/assets/prod-imgs/46.jpg";
+import Img47 from "@/assets/prod-imgs/47.jpg";
+import Img48 from "@/assets/prod-imgs/48.jpg";
+import Img49 from "@/assets/prod-imgs/49.jpg";
